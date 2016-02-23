5d8344200c0c12b436563e21e3df92858849b00f
Alin Roman <alinroman@192-168-0-101.rdsnet.ro>
2016-02-23 22:09:13 +0200

    Added jsdoc documentation.

--- libs/AuthProviderAbstract.js
+++ libs/AuthProviderAbstract.js
@@ -1,29 +1,59 @@
+/**
+ * @param storageProvider {StorageProviderAbstract}
+ * @constructor
+ */
 function AuthProviderAbstract(storageProvider) {
     this.storageProvider = storageProvider;
     this.protocol = 'generic';
     this.version = '1.0';
 }
 
+/**
+ * Returns the storage provider used to store the auth data
+ * @returns {StorageProviderAbstract}
+ */
 AuthProviderAbstract.prototype.getStorageProvider = function() {
     return this.storageProvider;
 };
 
+/**
+ * Returns a promise of auth tokens
+ * @param credentials {Object}
+ * @returns {Promise<Object>}
+ */
 AuthProviderAbstract.prototype.getAuthTokensAsync = function(credentials) {
     throw new Error('Not implemented.');
 };
 
+/**
+ * Returns the credentials key based on credentials object
+ * @param credentials {Object}
+ * @returns {String}
+ */
 AuthProviderAbstract.prototype.getCredentialsKey = function(credentials) {
     throw new Error('Not implemented.');
 };
 
+/**
+ * Returns a promise of a login url used to authenticate on an external service
+ * @returns {Promise<String>}
+ */
 AuthProviderAbstract.prototype.getLoginUrlAsync = function() {
     throw new Error('Not implemented.');
 };
 
+/**
+ * Returns the protocol used to authenticate
+ * @returns {String}
+ */
 AuthProviderAbstract.prototype.getProtocol = function() {
     return this.protocol;
 };
 
+/**
+ * Returns the version of the protocol used to authenticate
+ * @returns {String}
+ */
 AuthProviderAbstract.prototype.getVersion = function() {
     return this.version;
 };
